cbbccdad4aea949cdfb000f8953321ee66bca455
Aakash Mane <aakashmane5058@gmail.com>
2026-09-16 12:36:59 +0530

    feat: add schema

---
+++ backend/db.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const requirementSchema = new Schema({
+  // Fields - would be same for everything
+  eventName: { type: String, required: true },
+  eventType: { type: String, required: true },
+  isDateRange: { type: Boolean, default: false },
+  startDate: { type: Date, required: true },
+  endDate: { type: String, required: true },
+  venue: { type: String, default: "" },
+
+  // Categorise the requirements
+  category: {
+    type: String,
+    required: true,
+    enum: ["planner", "performer", "crew"],
+  },
+
+  details: {
+    type: Schema.Types.Mixed,
+    required: true,
+  },
+
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Requirement = mongoose.model("Requirement", requirementSchema);
+
+module.exports = { Requirement };
